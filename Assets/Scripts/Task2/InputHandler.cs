f1ce11e25efcc46a814ee1d8b4b32b900ff7713e
Katolikov Andrei <katolikov.andrei@mail.ru>
2025-03-27 21:02:16 +0700

    SceneIsDone
    Now go to the scripts

---
+++ Assets/Scripts/Task2/InputHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    private string HorizontalAxis = "Horizontal";
+    private string VerticalAxis = "Vertical";
+
+
+    private void Update()
+    {
+         
+    }
+
+    private void HandleInput()
+    {
+        
+    }
+}
